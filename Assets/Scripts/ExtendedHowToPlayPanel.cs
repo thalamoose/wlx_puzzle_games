bfe2599fd139083e117bf856145edd60bb0c2a5c
FunkyFingerJames <43827279+FunkyFingerJames@users.noreply.github.com>
2026-03-02 16:58:58 +0900

    Added sound manager, added some new graphics, Added HowToPlay panel script, Hooked up some new UIs on the front end

---
+++ Assets/Scripts/ExtendedHowToPlayPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExtendedHowToPlayPanel : HowToPlayPanel 
+{
+	//public Text howToPlayDescriptionTextLine2;
+	//public List<string> howToPlayLine1Text;
+	//public List<string> howToPlayLine2Text;
+
+	public override void NextHowToPlayPage ()
+	{
+		SoundManager.Instance.PlaySoundEffect (SoundManager.GENERIC_BUTTON, false);
+		base.NextHowToPlayPage ();
+	}
+
+	public override void PrevHowToPlayPage ()
+	{
+		SoundManager.Instance.PlaySoundEffect (SoundManager.GENERIC_BUTTON, false);
+		base.PrevHowToPlayPage ();
+	}
+
+	protected override void SetHowToPlayPageText ()
+	{
+		//howToPlayPageDescriptionText.text = howToPlayLine1Text [currentHowToPlayPageIndex];
+		//howToPlayDescriptionTextLine2.text = howToPlayLine2Text [currentHowToPlayPageIndex];
+	}
+}
